ef541011ebe6cd7472a9b61e9e2ab6e6f2de08f2
Daniel <danicunha97@gmail.com>
2020-10-02 23:36:54 +0100

    Changes on KLM model and js recommendations button

--- src/main/java/ResourcesTimeModel.java
+++ src/main/java/ResourcesTimeModel.java
@@ -2,6 +2,9 @@ public class ResourcesTimeModel {
     public String firstRsrc;
     public String secondRsrc;
     public float diffResourceTime;
+    public float diffResourceTime2;
+    public double median;
+    public float percentil;
 
     public ResourcesTimeModel() {
     }
